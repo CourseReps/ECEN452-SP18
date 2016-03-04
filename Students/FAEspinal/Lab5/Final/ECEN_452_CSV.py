63a3392110e0b4e177a2077df743b079d35d27ac
Francisco Espinal <FAEspinal@dhcp-10-202-129-76.tamulink.tamu.edu>
2016-03-03 22:22:24 -0600

    Lab5 Update

---
+++ Students/FAEspinal/Lab5/Final/ECEN_452_CSV.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import csv
+
+#***Note: In order to use this code, all of the columns in the .csv file must have the same length. If your columns have
+# different lengths, simply repeat the last value in each of the shorter columns until they are all the same size.
+
+#Initialize arrays for Freq, Measured, Simulated
+Freq = []
+Measured = []
+Simulated = []
+
+
+##Read .csv data file
+#replace quoted text below with filepath to your .csv file
+with open('PIN_S11_dB_OFF.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        #items in '' below need to exactly match the entry in the first row of the columns in the .csv file
+        #edit/add additional lines as needed for each column of data
+        Freq.append(float(row['Freq']))
+        Measured.append(float(row['Measured']))
+        Simulated.append(float(row['Simulated']))
+
+##Plotting
+plt.figure(1) #initialize plot1
+ax1 = plt.subplot(111) #create axes handle for plot1
+ax1.plot(Freq, Measured, '-b', label="Measured Data") #plot y1 vs. x, solid-blue, add lable for legend
+ax1.plot(Freq, Simulated, '-r', label="Simulated Data") #plot y2 vs. x, solid-red, add lable for legend
+ax1.set_xlim(min(Freq), max(Freq)) #set x-axis limits
+ax1.legend(loc=4) #add legend at location #4 (bottom-right corner)
+plt.grid(b=True, which='both', color='0.65', linestyle='-') #add solid grey gridlines
+plt.title('PIN Switch OFF S11 Plot (dB)') #add plot title
+plt.xlabel('Frequency [GHz]') #add x-axis title
+plt.ylabel('Magnitude') #add y-axis title
+
+
+plt.show() #required to display plots
--- Students/FAEspinal/Lab5/Final/README.md
+++
@@ -1,5 +0,0 @@
-# Identity
-
-* Name: Francisco A. Espinal 
-* GitHubID: FAEspinal
-* NetID: FAEspinal
