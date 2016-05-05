296f104df3245506ab15ec6cd976c8c08951335a
Hong Pan <hongpan0507@gmail.com>
2016-05-05 00:11:35 -0500

    labs

--- Students/hongpan0507/Lab10/MeasuredData/Friday/mod_plot.py
+++ Students/hongpan0507/Lab10/MeasuredData/Friday/mod_plot.py
@@ -7,9 +7,11 @@ import numpy as np
 def meas_rect_plot(file_name, data, save_name):
     fig = plot.figure(1, figsize=(12, 6), dpi=80, facecolor='w', edgecolor='k')
     ax0 = plot.subplot(111)
+    color = iter(cm.rainbow(np.linspace(0, 1, len(data))))
     for i in range(1, len(data)):
-        ax0.plot(data[0], data[i], label=file_name[i-1])
+        ax0.plot(data[0], data[i], color=next(color), label=file_name[i-1])
     # ax0.axis([0.2, 5, -50, 5])     # [xmin, xmax, ymin, ymax]
+
     plot.xlabel('Frequency (GHz)')
     plot.ylabel('Dielectric Constant')
     plot.title('Dielectric Probe Measurement')
@@ -17,3 +19,4 @@ def meas_rect_plot(file_name, data, save_name):
     plot.subplots_adjust(left=0.1, right=0.7, top=0.9, bottom=0.1, hspace=0, wspace=0)
     plot.savefig('plotting/' + save_name, dpi=200, facecolor='w', edgecolor='k')
     plot.show()
+
