4ff198ac4c4d81310fc9df3402b5c5f58d1ed228
Hong Pan <hongpan0507@gmail.com>
2016-05-04 23:17:35 -0500

    labs

--- Students/hongpan0507/Lab10/MeasuredData/Friday/main_plotting.py
+++ Students/hongpan0507/Lab10/MeasuredData/Friday/main_plotting.py
@@ -1,9 +1,12 @@
 from mod_read_data import read_measured_data
 from mod_plot import meas_rect_plot
 
-file_name = ['Acetone','DI_Water','DI_water_20MHz_20GHz', 'Ethylene_Glycol','Hydrocal','Isopropyl_Alcohol',
-                 'Pine_sol','Silicone_Fluid','Simple_Green','WD_40']
-
+file_name = ['Acetone', 'DI_Water', 'Ethylene_Glycol', 'Glass_Cleaner', 'Hydrocal',
+             'Isopropyl_Alcohol', 'Pine_sol', 'Silicone_Fluid', 'Simple_Green', 'WD_40']
 data = read_measured_data(file_name)
-
 meas_rect_plot(file_name, data)
+
+file_name = ['DI_water_20MHz_20GHz']
+data = read_measured_data(file_name)
+meas_rect_plot(file_name, data)
+
